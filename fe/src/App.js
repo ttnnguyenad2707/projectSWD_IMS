201c57a0f67a0998f9178e61acf05469ee8ee303
lehoanglong2311 <lehoanglong2311@gmail.com>
2023-10-29 14:23:25 +0700

    fix lai router

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import appRouter from './router/index.router';
+import { RouterProvider } from 'react-router-dom'
 
 function App() {
   return (
     <div>
+  <RouterProvider router={appRouter} />
       
     </div>
   );
--- fe/src/component/Table.component.js
+++ fe/src/component/Table.component.js
@@ -2,18 +2,13 @@ import { useEffect, useState } from "react"
 import axios from 'axios'
 import { URL_SERVER } from "../config/database";
 const Table = () => {
-    const [data,setData] = useState([]);
-    console.log(data);
-    useEffect(()=>{
-        axios.get(`${URL_SERVER}/student`).then(data => setData(data.data))
-    },[])
+   
 
     return (
-        <div><ul>{data.map((data,index) => {
-            return (
-                <li key={index}>{data.name} - {data.gender}</li>
-            )
-        })}</ul></div>
+        
+        <>
+        <h1>hello</h1>
+        </>
     )
 }
 

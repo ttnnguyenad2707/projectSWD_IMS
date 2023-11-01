09f7a337881d81e7ec32d43a15e8acdfdcab9716
tudda <tentufancr7@gmail.com>
2023-11-01 13:47:06 +0700

    .

--- fe/src/router/index.router.js
+++ fe/src/router/index.router.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Table from "../component/Table.component.js";
-
+import ClassList from "../component/ClassList.component.js";
 const appRouter = createBrowserRouter([
-    {
-        path: '/home',
-        element: <Table/>
-    }
-])
-export default appRouter
+  {
+    path: "/home",
+    element: <Table />,
+  },
+  {
+    path: "/class_list",
+    element: <ClassList />,
+  },
+]);
+export default appRouter;

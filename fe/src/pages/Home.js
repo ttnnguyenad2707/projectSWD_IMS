7951e1d78163a264c5de354478f69dbc3f168303
tudda <tentufancr7@gmail.com>
2023-11-06 14:42:58 +0700

    .

--- fe/src/pages/Home.js
+++ fe/src/pages/Home.js
@@ -1,36 +1,79 @@
-import React from 'react';
-import { useNavigate, useOutletContext } from 'react-router-dom';
-import { Col, Container, Row } from 'react-bootstrap'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    const [user] = useOutletContext();
-    const nav = useNavigate();
+  const [user] = useOutletContext();
+  const nav = useNavigate();
 
+  return (
+    <div>
+      <p>{user?.email}</p>
 
-    return (
-        <div>
-            <p>{user?.email}
-            </p>
-
-            <Container fluid>
-                <Row>
-                    <Col md={2}>
-                        <div className='gap-3 d-flex flex-column'>
-                            {user?.Role === "teacher" ? <button className='btn btn-primary' onClick={() => { nav("/projectList") }}>Project</button> : ""}
-                            {user?.Role === "admin" ? <Link className='btn btn-primary' to='/systemsetting'> Setting </Link> : ""}
-                            {user?.Role === "admin" ? <Link className='btn btn-primary' to='/admin/subject'> Subject </Link> : ""}
-                            {user?.Role === "student" ? <Link className='btn btn-primary' to='/issues'> Issue </Link> : ""}
-                            {user?.Role === "teacher" ? <Link className='btn btn-primary' to='/classlist'> Class </Link> : ""}
-                        </div>
-                    </Col>
-                    <Col>
-
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+      <Container fluid>
+        <Row>
+          <Col md={2}>
+            <div className='gap-3 d-flex flex-column'>
+              {user?.Role === "teacher" ? (
+                <button
+                  className='btn btn-primary'
+                  onClick={() => {
+                    nav("/projectList");
+                  }}
+                >
+                  Project
+                </button>
+              ) : (
+                ""
+              )}
+              {user?.Role === "admin" ? (
+                <Link className='btn btn-primary' to='/systemsetting'>
+                  {" "}
+                  Setting{" "}
+                </Link>
+              ) : (
+                ""
+              )}
+              {user?.Role === "admin" ? (
+                <Link className='btn btn-primary' to='/admin/subject'>
+                  {" "}
+                  Subject{" "}
+                </Link>
+              ) : (
+                ""
+              )}
+              {user?.Role === "student" ? (
+                <Link className='btn btn-primary' to='/issues'>
+                  {" "}
+                  Issue{" "}
+                </Link>
+              ) : (
+                ""
+              )}
+              {user?.Role === "teacher" ? (
+                <Link className='btn btn-primary' to='/classlist'>
+                  {" "}
+                  Class{" "}
+                </Link>
+              ) : (
+                ""
+              )}
+              {user?.Role === "admin" ? (
+                <Link className='btn btn-primary' to='/class/create'>
+                  {" "}
+                  Class{" "}
+                </Link>
+              ) : (
+                ""
+              )}
+            </div>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
-export default Home
+export default Home;
